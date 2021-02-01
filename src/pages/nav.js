13865f27b633b633335a37fe3b3680b012e2a963
TressOhi <fortressenebe@gmail.com>
2021-02-01 13:46:46 +0100

    basic setup

--- src/pages/nav.js
+++ src/pages/nav.js
@@ -1,1 +1,18 @@
 import React, {Component} from 'react'
+
+class Nav extends Component{
+    constructor(props){
+        super(props)
+    }
+
+    reder(){
+        return(
+            <div>
+                
+            </div>
+        );
+    }
+
+}
+
+export default Nav;
